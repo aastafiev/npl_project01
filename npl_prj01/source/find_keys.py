1178641a8f0fcddc9cb61b68597d18c62c13f99f
aastafiev <aastafiev@gmail.com>
2017-03-26 03:43:43 +0300

    Comments in notes.md (Aleksey Astafiev 26.03.2017 02:07)

--- npl_prj01/source/find_keys.py
+++ npl_prj01/source/find_keys.py
@@ -38,58 +38,88 @@ def url2domain(url):
 
 
 def get_meta_content(html):
-    out = []
+    out = list()
     soup = BeautifulSoup(html, "lxml")
     for tag in soup.find_all("meta"):
-        if tag.has_attr("name") and tag["name"] in st.META_NAMES:
+        if tag.has_attr("name") and tag["name"].lower() in st.META_NAMES and tag["content"].strip():
             out.append(tag["content"])
     return out
 
 
-def get_all_meta_content(urls):
-    out = []
+def get_all_meta_content(urls_ts):
+    out = list()
+    lost_urls = list()
+    domain_url = list()
 
-    for url in urls:
+    buffer_urls = list()
+
+    for (url, timestamp) in urls_ts:
         try:
-            no_exception = True
-            response = urllib2.urlopen(url)
+            domain, url_scheme = url2domain(url)
+            domain_url.extend([(domain, url, timestamp)])
+            url_new = '://'.join([url_scheme, domain])
+            if url_new not in buffer_urls:
+                # Сначала по домену
+                buffer_urls.extend([url_new])
+                print "Trying NEW URL %s" % url_new
+                response = urllib2.urlopen(url_new)
+                meta_info = get_meta_content(response)
+                if meta_info:
+                    out.extend(meta_info)
+                    print "Got meta"
+                else:
+                    lost_urls.extend([(url_new, 'empty_meta')])
+                    print "Empty meta"
+
+            if url not in buffer_urls:
+                # Теперь по основной ссылке
+                buffer_urls.extend([url])
+                print "Trying %s" % url
+                response = urllib2.urlopen(url)
+                meta_info = get_meta_content(response)
+                if meta_info:
+                    out.extend(meta_info)
+                    print "Got meta"
+                else:
+                    lost_urls.extend([(url, 'empty_meta')])
+                    print "Empty meta"
         # except urllib2.HTTPError:
         # except urllib2.URLError, e:
         #     print 'URLError = ' + str(e.reason)
         except Exception:
-            url, url_scheme = url2domain(url)
-            response = urllib2.urlopen("://".join([url_scheme, url]))
-            no_exception = False
-
-        out.extend(get_meta_content(response))
-
-        if no_exception:
-            url, url_scheme = url2domain(url)
-            response = urllib2.urlopen("://".join([url_scheme, url]))
-            out.extend(get_meta_content(response))
-    return list(set(out))
+            lost_urls.extend([(url, 'bad_url')])
+            print "Bad url"
+    return list(set(out)), domain_url, lost_urls
 
 
-def gen_uid_meta_file():
+def parse_to_files(nrows=None):
     fields = ['uid', 'user_json']
-    df = pd.read_csv(st.MAIN_FILE_PATH, sep='\t', skipinitialspace=True, usecols=fields, nrows=5)
-    # urls = list()
+    df = pd.read_csv(st.MAIN_FILE_PATH, sep='\t', skipinitialspace=True, usecols=fields, nrows=nrows)
+    urls_ts = list()
 
-    with open(st.UID_META_FILE_PATH, 'w') as um_file:
+    with open(st.UID_META_FILE_PATH, 'w') as um_file,\
+            open(st.UID_LOST_URLS_FILE_PATH, 'w') as lu_file,\
+            open(st.UID_DOMAIN_URL_TIMESTAMP_FILE_PATH, 'w') as dut_file:
         for row in df.itertuples():
+            uid = row[1]
             uid_visits = json.loads(row[2])
             for v in uid_visits['visits']:
-                um_file.write("%s\t%s\n" % (row[1], v['url'].encode('utf-8'))) # urls.extend(v['url'])
+                timestamp = v['timestamp']
+                urls_ts.extend([(v['url'], timestamp)])
+
+            print "\nUser %s\n------------------" % uid
+            meta_data, domains_urls, lost_urls = get_all_meta_content(urls_ts)
+            for md in meta_data:
+                um_file.write("%s\t%s\n" % (uid, md.encode('utf-8')))
+            for (dom, url, ts) in domains_urls:
+                dut_file.write("%s\t%s\t%s\t%s\n" % (uid, dom.encode('utf-8'), url.encode('utf-8'), ts))
+            for (url, err) in lost_urls:
+                lu_file.write("%s\t%s\t%s\n" % (uid, url.encode('utf-8'), err))
 
 
 def main():
-    # res = get_all_meta_content(urls)
+    parse_to_files(1)
 
-    # df = pd.read_csv(st.FILE_PATH, sep='\t')
-    gen_uid_meta_file()
-
-    # for i in res:
-    #     print i
 
 if __name__ == "__main__":
     main()
--- npl_prj01/source/settings.py
+++ npl_prj01/source/settings.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-SEP = '\t'
+# SEP = '\t'
 DATA_DIR = '../data'
 MAIN_FILENAME = 'gender_age_dataset.txt'
 UID_META_FILENAME = 'uid_meta.txt'
+UID_DOMAIN_URL_TIMESTAMP_FILENAME = 'uid_domain_url_timestamp.txt'
+UID_LOST_URLS_FILENAME = 'uid_lost_url.txt'
+
+
 MAIN_FILE_PATH = '/'.join([DATA_DIR, MAIN_FILENAME])
 UID_META_FILE_PATH = '/'.join([DATA_DIR, UID_META_FILENAME])
+UID_LOST_URLS_FILE_PATH = '/'.join([DATA_DIR, UID_LOST_URLS_FILENAME])
+UID_DOMAIN_URL_TIMESTAMP_FILE_PATH = '/'.join([DATA_DIR, UID_DOMAIN_URL_TIMESTAMP_FILENAME])
+
 
 META_NAMES = ['description',
               'og:description',
