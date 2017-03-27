dac2bb35a90710ae5bfaa6d747d067448bee5d92
Dima Beloborodov <d.beloborodov@gmail.com>
2017-03-27 21:23:35 +0300

    Переписан find_keys.py
     - удалено использование settings.py (параметры передаются через аргументы командной строки)
     - добавлено асинхронное выполнение запросов

--- npl_prj01/source/find_keys.py
+++ npl_prj01/source/find_keys.py
@@ -10,18 +10,25 @@
 # user_json - поле json, в котором содержатся записи о посещении сайтов этим пользователем (url, timestamp).
 
 
-import settings as st
-import pandas as pd
-import json
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import os
-# from urlparse import urlparse
-# from urllib import unquote
 import re
+import sys
+import json
+import urllib2
+import argparse
+import grequests
+
+import pandas as pd
 from urlparse import urlparse
 from bs4 import BeautifulSoup
-import urllib2
+
+
+META_NAMES = ['description',
+              'og:description',
+              'twitter:description'
+              'keywords',
+              'title',
+              'og:title',
+              'twitter:title']
 
 
 def url2domain(url):
@@ -34,96 +41,101 @@ def url2domain(url):
 
     if netloc is not None:
         return str(netloc.encode('utf8')).strip(), parsed_url.scheme
-    return None
 
 
 def get_meta_content(html):
-    out = list()
-    soup = BeautifulSoup(html, "lxml")
-    for tag in soup.find_all("meta"):
-        if tag.has_attr("name") and tag["name"].lower() in st.META_NAMES and tag["content"].strip():
-            out.append(tag["content"].strip())
-    return out
+    return [tag["content"].strip()
+            for tag in BeautifulSoup(html, "lxml").find_all("meta")
+            if tag.has_attr("name")
+            if tag["name"].lower() in META_NAMES
+            if tag["content"].strip()]
 
 
-def get_all_meta_content(urls_ts, count):
-    out = list()
-    lost_urls = list()
-    domain_url = list()
+def get_all_meta_content(urls_ts, count, timeout=None):
+    out = []
+    lost_urls = []
+    domain_url = []
+    urls = []
 
-    buffer_urls = list()
+    for url, timestamp in urls_ts:
+        domain, url_scheme = url2domain(url)
+        domain_url.extend([(domain, url, timestamp)])
+        new_url = '://'.join([url_scheme, domain])
+        if not url in urls:
+            urls.append(url)
+        if not new_url in urls:
+            urls.append(new_url)
 
-    for (url, timestamp) in urls_ts:
+    requests = (grequests.get(u) for u in urls)
+    responses = grequests.map(requests=requests, gtimeout=timeout)
+
+    for i, response in enumerate(responses):
         try:
             count += 1
-            domain, url_scheme = url2domain(url)
-            domain_url.extend([(domain, url, timestamp)])
-            url_new = '://'.join([url_scheme, domain])
-            if url_new not in buffer_urls:
-                # Сначала по домену
-                buffer_urls.extend([url_new])
-                print "%d: Trying NEW URL %s" % (count, url_new)
-                response = urllib2.urlopen(url_new)
-                meta_info = get_meta_content(response)
-                if meta_info:
-                    out.extend(meta_info)
-                    print "Got meta"
-                else:
-                    lost_urls.extend([(url_new, 'empty_meta')])
-                    print "Empty meta"
-
-            if url not in buffer_urls:
-                # Теперь по основной ссылке
-                buffer_urls.extend([url])
-                print "%d: Trying %s" % (count, url)
-                response = urllib2.urlopen(url)
-                meta_info = get_meta_content(response)
-                if meta_info:
-                    out.extend(meta_info)
-                    print "Got meta"
-                else:
-                    lost_urls.extend([(url, 'empty_meta')])
-                    print "Empty meta"
-        # except urllib2.HTTPError:
-        # except urllib2.URLError, e:
-        #     print 'URLError = ' + str(e.reason)
-        except Exception:
-            lost_urls.extend([(url, 'bad_url')])
-            print "Bad url"
+            meta_info = get_meta_content(response._content)
+            if meta_info:
+                out.extend(meta_info)
+                print "%s:\tGot meta:\t%s" % (count, urls[i])
+            else:
+                lost_urls.append((url, 'empty_meta'))
+                print "%s:\tEmpty meta:\t%s" % (count, urls[i])
+        except Exception as ex:
+            lost_urls.append((url, 'empty_meta'))
+            print '%d:\tERROR:\t%s - %s' % (count, urls[i], ex)
     return list(set(out)), domain_url, lost_urls, count
 
 
-def parse_to_files(nrows=None):
+def parse_to_files(in_file_path,
+                   meta_file_path,
+                   domain_urls_timestamp_file_path,
+                   lost_urls_file_path,
+                   timeout=None,
+                   nrows=None):
     fields = ['uid', 'user_json']
-    df = pd.read_csv(st.MAIN_FILE_PATH, sep='\t', skipinitialspace=True, usecols=fields, nrows=nrows)
-    urls_ts = list()
+    df = pd.read_csv(in_file_path, sep='\t', skipinitialspace=True, usecols=fields, nrows=nrows)
 
-    with open(st.UID_META_FILE_PATH, 'w') as um_file,\
-            open(st.UID_LOST_URLS_FILE_PATH, 'w') as lu_file,\
-            open(st.UID_DOMAIN_URL_TIMESTAMP_FILE_PATH, 'w') as dut_file:
+    with open(meta_file_path, 'w') as um_file,\
+         open(lost_urls_file_path, 'w') as lu_file,\
+         open(domain_urls_timestamp_file_path, 'w') as dut_file:
         count = 0
-        for row in df.itertuples():
-            uid = row[1]
-            uid_visits = json.loads(row[2])
-            for v in uid_visits['visits']:
-                timestamp = v['timestamp']
-                urls_ts.extend([(v['url'], timestamp)])
-
+        for _, uid, user_json in df.itertuples():
             print "\nUser %s\n------------------" % uid
-            meta_data, domains_urls, lost_urls, count = get_all_meta_content(urls_ts, count)
+
+            user_json = json.loads(user_json)
+            urls_ts = [(v['url'], v['timestamp']) for v in user_json['visits']]
+            meta_data, domains_urls, lost_urls, count = get_all_meta_content(urls_ts, count, timeout)
+
             for md in meta_data:
                 um_file.write("%s\t%s\n" % (uid, ' '.join(md.encode('utf-8', 'ignore').split())))
             for (dom, url, ts) in domains_urls:
                 dut_file.write("%s\t%s\t%s\t%s\n" %
-                               (uid, dom.encode('utf-8', 'xmlcharrefreplace'),
-                                url.encode('utf-8', 'xmlcharrefreplace'), ts))
+                    (uid, dom.encode('utf-8', 'xmlcharrefreplace'), url.encode('utf-8', 'xmlcharrefreplace'), ts))
             for (url, err) in lost_urls:
                 lu_file.write("%s\t%s\t%s\n" % (uid, url.encode('utf-8', 'xmlcharrefreplace'), err))
 
 
-def main():
-    parse_to_files()
-
-
 if __name__ == "__main__":
-    main()
+    arg_parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    arg_parser.add_argument('--in-file-path',
+                            default='gender_age_dataset.txt',
+                            help='input file path')
+    arg_parser.add_argument('--meta-file-path',
+                            default='uid_meta.txt',
+                            help='meta file path')
+    arg_parser.add_argument('--lost-urls-file-path',
+                            default='uid_lost_url.txt',
+                            help='lost urls file path')
+    arg_parser.add_argument('--domain-urls-timestamp-file-path',
+                            default='uid_domain_url_timestamp.txt',
+                            help='domain urls timestamp file path')
+    arg_parser.add_argument('--nrows',
+                            type=int,
+                            default=None,
+                            help='rows count to get from in file')
+    arg_parser.add_argument('--timeout',
+                            type=int,
+                            default=None,
+                            help='timeout for all requests of one uid')
+
+    args = arg_parser.parse_args()
+    parse_to_files(**args.__dict__)
