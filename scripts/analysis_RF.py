ab2705d72a21a070764d4e5a3abd2647b5473833
aastafiev <aastafiev@gmail.com>
2017-04-08 15:21:11 +0300

    Changed data preparations, added functions to run different classification algorithms.

--- scripts/analysis_RF.py
+++ scripts/analysis_RF.py
@@ -2,14 +2,20 @@
 # -*- coding: utf-8 -*-
 
 # import nltk
-# import scipy
 import numpy as np
 import pandas as pd
 from time import time
+import re
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.ensemble import RandomForestClassifier
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.naive_bayes import BernoulliNB
+from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
+
+from nltk.corpus import stopwords
+import pymorphy2
+from nltk.stem import WordNetLemmatizer
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 
@@ -18,7 +24,7 @@ from sklearn.feature_selection import chi2
 # from sklearn.metrics.pairwise import cosine_similarity
 
 
-def prepare_dfs(gen_file_path, in_file_path):
+def prepare_dfs(gen_file_path, in_file_path, feature):
     print "Load to DataFrame gen_file_path"
     gen_df = pd.read_csv(gen_file_path,
                          encoding='utf-8',
@@ -27,8 +33,6 @@ def prepare_dfs(gen_file_path, in_file_path):
                          usecols=['gender', 'age', 'uid']
                          )
     gen_train_df = gen_df[~((gen_df['gender'] == '-') & (gen_df['age'] == '-'))]
-    gen_train_df_uids = gen_train_df['uid'].unique()
-
     gen_test_df = gen_df[(gen_df['gender'] == '-') & (gen_df['age'] == '-')]
     gen_test_df_uids = gen_test_df['uid'].unique()
 
@@ -39,116 +43,96 @@ def prepare_dfs(gen_file_path, in_file_path):
                           skipinitialspace=True,
                           )
 
-    meta_train_df = meta_df[meta_df['uid'].isin(gen_train_df_uids.tolist())]
+    print "Preparing Train DataFrame"
+    meta_train_df = pd.merge(meta_df, gen_train_df, on='uid', sort=False)
+    meta_train_series = meta_train_df.groupby(feature)['meta']\
+        .apply(lambda x: u' '.join([unicode(ss).replace('&nbsp', ' ').replace('&quot', '"')
+                                   .replace('&laquo', '"').replace('&raquo', '"') for ss in x.tolist()]))
+    meta_train_df = pd.DataFrame(meta_train_series, index=meta_train_series.index, columns=['meta'])
+
+    print "Preparing Test DataFrame"
     meta_test_df = meta_df[meta_df['uid'].isin(gen_test_df_uids.tolist())]
+    meta_test_series = meta_test_df.groupby('uid')['meta']\
+        .apply(lambda x: u' '.join([unicode(ss).replace('&nbsp', ' ').replace('&quot', '"')
+                                   .replace('&laquo', '"').replace('&raquo', '"') for ss in x.tolist()]))
+    meta_test_df = pd.DataFrame(meta_test_series, index=meta_test_series.index, columns=['meta'])
 
-    t0 = time()
-    print "Transform Train DataFrame"
-    # Transform Train/Test DataFrame
-    tmeta_train_df = pd.DataFrame(columns=('uid', 'meta'))
-    tmeta_test_df = pd.DataFrame(columns=('uid', 'meta'))
-    i = 0
-    for u_uid in gen_train_df_uids:
-        df1 = meta_train_df.loc[meta_train_df['uid'] == u_uid, 'meta']
-        mstr = u' '.join([unicode(ss) for ss in df1.tolist()])
-        mstr = mstr.replace('&nbsp', ' ')
-        mstr = mstr.replace('&quot', '"')
-        tmeta_train_df.loc[i] = [u_uid, mstr]
-        print u'TRAIN UID: %s\tIter: %d' % (u_uid, i)
-        i += 1
-    tmeta_train_df['meta'].replace('', np.nan, inplace=True)
-    tmeta_train_df.dropna(subset=['meta'], inplace=True)
-
-    print "Transform Test DataFrame"
-    i = 0
-    for u_uid in gen_test_df_uids:
-        df1 = meta_test_df.loc[meta_test_df['uid'] == u_uid, 'meta']
-        mstr = u' '.join([unicode(ss) for ss in df1.tolist()])
-        mstr = mstr.replace('&nbsp', ' ')
-        mstr = mstr.replace('&quot', '"')
-        tmeta_test_df.loc[i] = [u_uid, mstr]
-        print u'TEST UID: %s\tIter: %d' % (u_uid, i)
-        i += 1
-    tmeta_test_df['meta'].replace('', np.nan, inplace=True)
-    tmeta_test_df.dropna(subset=['meta'], inplace=True)
-
-    tmeta_train_df.sort_values(by=['uid'], inplace=True)
-    tmeta_test_df.sort_values(by=['uid'], inplace=True)
-
-    train_time = time() - t0
-    print "Transform time: %0.3fs" % train_time
-
-    return gen_train_df, gen_test_df, meta_train_df, meta_test_df, tmeta_train_df, tmeta_test_df
+    # return gen_train_df, gen_test_df, meta_train_df, meta_test_df
+    return meta_train_df, meta_test_df
 
 
-def main(gen_file_path, in_file_path):
+class LemmaTokenizer(object):
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+        self.morph = pymorphy2.MorphAnalyzer()
+
+    def __call__(self, text):
+        tokenz = TfidfVectorizer().build_tokenizer()(text)
+        lemmas = []
+        for t in tokenz:
+            if len(t) > 2:
+                p = self.morph.parse(t)
+                if 'LATN' in p[0].tag: # and re.search('!\d+', p[0].normal_form)
+                    lemmas.append(self.wnl.lemmatize(t))
+                elif 'NUMB' in p[0].tag:
+                    continue
+                elif 'UNKN' in p[0].tag:
+                    continue
+                elif 'ROMN' in p[0].tag:
+                    continue
+                else:
+                    lemmas.append(p[0].normal_form)
+        return lemmas
+
+
+def vectorizing(meta_train_df, meta_test_df):
     print '_' * 80
-    print "Cooking data "
+    print "Vectorizing"
+    print "Train"
     t0 = time()
-    gen_train_df, gen_test_df, meta_train_df, meta_test_df, tmeta_train_df, tmeta_test_df = \
-        prepare_dfs(gen_file_path, in_file_path)
-    train_time = time() - t0
-    print "cooking time: %0.3fs" % train_time
-    print "Data ready"
-# Print out prepared data info
-#     print u'len(gen_train_df) = %d\nlen(gen_test_df) = %d\nlen(meta_train_df) = %d\n' \
-#           u'len(meta_test_df) = %d\nlen(tmeta_train_df) = %d\nlen(tmeta_test_df) = %d' % \
-#           (len(gen_train_df),
-#            len(gen_test_df),
-#            len(meta_train_df),
-#            len(meta_test_df),
-#            len(tmeta_train_df),
-#            len(tmeta_test_df))
 
-    # for _, u, tm in tmeta_train_df.itertuples():
-    #     print (u'%s\t%s' % (u, tm)).encode('utf-8')
-
-    uids_train = tmeta_train_df['uid'].tolist()
-    y_train = tmeta_train_df.index.values
-    # print y_train
-    # print type(y_train)
-    corpus_train = tmeta_train_df['meta'].tolist()
-    vectorizer = TfidfVectorizer() # ngram_range=(1, 2)
+    y_train = meta_train_df.index.values
+    corpus_train = meta_train_df['meta'].tolist()
+    stop_words = stopwords.words('english') + stopwords.words('russian') + ['ru', 'com']
+    vectorizer = TfidfVectorizer(tokenizer=LemmaTokenizer(), stop_words=stop_words)  # ngram_range=(1, 2)
     x_train = vectorizer.fit_transform(corpus_train)
-    feature_names = np.array(vectorizer.get_feature_names())
+    feature_names = np.asarray(vectorizer.get_feature_names())
 
-    ch2 = SelectKBest(chi2, k=1000)
-    x_train = ch2.fit_transform(x_train, y_train)
+    command_time = time() - t0
+    print "vectorizing train time: %0.3fs" % command_time
 
-    uids_test = tmeta_test_df['uid'].tolist()
-    y_test = tmeta_test_df.index.values
-    corpus_test = tmeta_test_df['meta'].tolist()
+    # print x_train.shape
+    # print x_train.toarray()
+    #
+    # ch2 = SelectKBest(chi2, k=10)
+    # x_train = ch2.fit_transform(x_train, y_train)
+
+    print "Test"
+    t0 = time()
+
+    y_test = meta_test_df.index.values
+    corpus_test = meta_test_df['meta'].tolist()
     x_test = vectorizer.transform(corpus_test)
 
-    x_test = ch2.transform(x_test)
+    command_time = time() - t0
+    print "vectorizing test time: %0.3fs" % command_time
+    # x_test = ch2.transform(x_test)
 
-# Print out x_train
-    # print X_train
+    # Print out x_train, x_test
+    print x_train.toarray()
     print x_train.shape
+
+    print x_test.toarray()
     print x_test.shape
-    # print y_test, uids_test
-    # row = 0
-    # line = u'-\t'
-    # for fn in feature_names:
-    #     line += u'%s\n' % fn
-    # print line.encode('utf-8')
 
-    # for uid in uids:
-    #     line = u'%s\t' % uid
-    #     col = 0
-    #     for _ in feature_names:
-    #         line += u'%s\t' % x_train[row, col]
-    #         col += 1
-    #     print line.encode('utf-8')
-    #     row += 1
+    return x_train, y_train, x_test, y_test, feature_names
 
-    # print np.count_nonzero(x_train)
 
-# Classification
-    clf = RandomForestClassifier(n_estimators=10, n_jobs=-1)
+def predicting(clf, clf_name, x_train, y_train, x_test, y_test):
+    # clf = MultinomialNB(alpha=.01)  # BernoulliNB(alpha=.01)
     print '_' * 80
-    print "Training: "
-    print "RandomForestClassifier"
+    print "%s" % clf_name
+    print "Training:"
     t0 = time()
     clf.fit(x_train, y_train)
     train_time = time() - t0
@@ -166,34 +150,39 @@ def main(gen_file_path, in_file_path):
 
     print "classification report:"
     # print uids_test[0]
-    print metrics.classification_report(y_test, pred) # , target_names=uids_test
+    print metrics.classification_report(y_test, pred)  # , target_names=uids_test
 
     print "confusion matrix:"
     print metrics.confusion_matrix(y_test, pred)
 
-    # dtm = vectorizer.fit_transform(raw_meta)
-    # vocab = np.array(vectorizer.get_feature_names())
-    # dtm = dtm.toarray()
-    # dist = 1 - cosine_similarity(dtm)
 
-    # mds = MDS(n_components=2, dissimilarity="precomputed", random_state=1)
-    # pos = mds.fit_transform(dist)
-    # xs, ys = pos[:, 0], pos[:, 1]
-    #
-    # for x, y in zip(xs, ys):
-    #     # color = 'orange' if "Austen" in name else 'skyblue'
-    #     plt.scatter(x, y)
-    #     plt.text(x, y)
-    #
-    # for x, y in zip(xs, ys):
-    #     # color = 'orange' if "Austen" in name else 'skyblue'
-    #     plt.scatter(x, y)
-    #     # plt.text(x, y)
-    # plt.show()
-    #
-    # linkage_matrix = ward(dist)
-    # dendrogram(linkage_matrix, orientation="right")
+def main(gen_file_path, in_file_path, feature):
+    print '_' * 80
+    print "Cooking data for feature '%s'" % feature
+    t0 = time()
+
+    meta_train_df, meta_test_df = prepare_dfs(gen_file_path, in_file_path, feature)
+
+    command_time = time() - t0
+    print "cooking data time: %0.3fs" % command_time
+# Print out prepared data info
+#     print u'len(gen_train_df) = %d\nlen(gen_test_df) = %d\nlen(meta_train_df) = %d\n' \
+#           u'len(meta_test_df) = %d\nlen(tmeta_train_df) = %d\nlen(tmeta_test_df) = %d' % \
+#           (len(gen_train_df),
+#            len(gen_test_df),
+#            len(meta_train_df),
+#            len(meta_test_df),
+#            len(tmeta_train_df),
+#            len(tmeta_test_df))
+
+# Vectorizing
+    x_train, y_train, x_test, y_test, feature_names = vectorizing(meta_train_df, meta_test_df)
+
+# Classification
+#     clf = RandomForestClassifier(n_estimators=10, n_jobs=-1)
+    clf = MultinomialNB(alpha=.01) # BernoulliNB(alpha=.01)
+    predicting(clf, 'MultinomialNB', x_train, y_train, x_test, y_test)
 
 if __name__ == "__main__":
     main('/Users/usual/PycharmProjects/npl_project01/data/gender_age_dataset.txt',
-         '/Users/usual/PycharmProjects/npl_project01/data/csv/uid_meta_fixed.csv')
+         '/Users/usual/PycharmProjects/npl_project01/data/csv/uid_meta_fixed.csv', 'age')
